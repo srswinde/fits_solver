e4f71ae61d0031335a0e207329e6e9317ebee87e
scott <scottswindell@email.arizona.edu>
2015-12-11 16:00:07 -0700

    I don't remember what I did.

--- compile.py
+++ compile.py
@@ -2,8 +2,11 @@ from astropy.io import fits
 from astro.angles import RA_angle, Dec_angle, Deg10
 import os
 import json
+import argparse
+
+
 example = {
-"amp11Dec": "+85:05:02.77",
+		  "amp11Dec": "+85:05:02.77",
         "amp11RA": "08:21:46.83",
         "amp6Dec": "+84:34:01.54",
         "amp6RA": "08:58:31.27",
@@ -20,41 +23,74 @@ example = {
 
 
 
-ext1names = [name for name in os.listdir('ext1') if name[-3:] == 'wcs']
-infolist = []
 
-for name in ext1names:
-	if name in os.listdir('ext2'):
-		bname = name[:-4]
-		ext1 = fits.open( 'ext1/{0}'.format(name) )
-		ext2 = fits.open( 'ext2/{0}'.format(name) )
-		
-		d1 = fits.open( 'ext1/{0}.new'.format(bname) )
 
-		dec = ext1[0].header['crval2']+(ext1[0].header['crval2']- ext2[0].header['crval2'])/2.0
-		ra = ext1[0].header['crval1']+(ext1[0].header['crval1']- ext2[0].header['crval1'])/2.0
+def get_info():
+	for name in ext1names:
+		if name in os.listdir('ext2'):
+			bname = name[:-4]
+			ext1 = fits.open( 'ext1/{0}'.format(name) )
+			ext2 = fits.open( 'ext2/{0}'.format(name) )
 		
-		ra = RA_angle( Deg10( ra ) )
-		dec = Dec_angle( Deg10( dec ) )
+			d1 = fits.open( 'ext1/{0}.new'.format(bname) )
+
+			dec = ext1[0].header['crval2']+(ext1[0].header['crval2']- ext2[0].header['crval2'])/2.0
+			ra = ext1[0].header['crval1']+(ext1[0].header['crval1']- ext2[0].header['crval1'])/2.0
+		
+			ra = RA_angle( Deg10( ra ) )
+			dec = Dec_angle( Deg10( dec ) )
 	
-		obsRA = d1[0].header['ra']
-		obsDec =  d1[0].header['dec']
-		obsHA =  d1[0].header['ha']
-		obsLST = d1[0].header['lst-obs']
+			obsRA = d1[0].header['ra']
+			obsDec =  d1[0].header['dec']
+			obsHA =  d1[0].header['ha']
+			obsLST = d1[0].header['lst-obs']
 
-		info = {
-			"obsDec"	: 	obsDec,
-			"obsHA"		: 	obsHA,
-			"obsRA"		: 	obsRA,
-			'lst'		:	obsLST,
-			'name'		:	bname,
-			'midpointDec'	:	str(dec),
-			'midpointRA'	:	str(ra),
+			info = {
+				"obsDec"			: 	obsDec,
+				"obsHA"			: 	obsHA,
+				"obsRA"			: 	obsRA,
+				'lst'				:	obsLST,
+				'name'			:	bname,
+				'midpointDec'	:	str(dec),
+				'midpointRA'	:	str(ra),
 			
-		}
-		infolist.append(info)
+			}
+			infolist.append(info)
 
 			
 		
-json.dump( infolist, open('data.json','w'), sort_keys=True, indent=4 )
+	json.dump( infolist, open('data.json','w'), sort_keys=True, indent=4 )
+
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(
+	description=
+"""compile.py:
+This is a quick and dirty way to gather astrometric data from astrometry.net style solutions 
+and make that information available for a
+
+	Auther	:	Scott Swindell
+	Date		:	12 10 2015"""
+	)
+	
+
+	parser.add_argument('-o',  default='tpoint.json', help='output json file')
+
+	
+
+	parser.add_argument( 'path',  help='Directory of the solution files' )
+	
+
+	args = parser.parse_args()
+	
+	files = os.listdir( args.path )
+	for fname in files:
+		if fname[-3:] == 'wcs':
+			
+
+
+
+
+
 
--- imsolve.py
+++ imsolve.py
@@ -3,18 +3,32 @@
 from astropy.io import fits
 import sys
 import os
+import argparse
 
-def do_astrometry(fname, params={}, flags=[]):
+
+default_params = {
+		'radius':5,
+		'scale-units': 'app',
+		'scale-low':0.1,
+		'scale-high':2,
+}
+
+default_flags =  ['overwrite', 'crpix-center']
+
+def astrometry_cmd(fname, params={}, flags=[]):
+
+	
 	args = ''
+	
 	for name, val in params.iteritems():
-		print name
+
 		if len(name) == 1:
 			args+=" -{0} {1} ".format(name, val)
 		else:
 			args+=" --{0} {1} ".format(name, val)
 
 	for flag in flags:
-		print flag
+		
 		if len(flag) == 1:
 			args+=" -{0} ".format(flag)
 		else:
@@ -24,37 +38,96 @@ def do_astrometry(fname, params={}, flags=[]):
 
 	return "solve-field {0} {1}".format( args, fname )
 	
-if len(sys.argv) < 2:
-	print "idiot"
 
-	sys.exit()
 
-if  not os.path.exists('anal'):
+
+
+def getazcamradec(fd):
+	return (fd[0].header['ra'], fd[0].header['dec'])
 	
-	os.mkdir('anal')
+
 	
-for num in range(12,130):
-	#fname = sys.argv[1]
-	fname = "pointing{0:04d}.fits".format(num)
-	print fname
-	fd = fits.open(fname)
-
-	p={
-
-		'ra':fd[0].header['ra'],
-		'dec':fd[0].header['dec'],
-		'radius':5,
-		'scale-units': 'app',
-		'scale-low':0.1,
-		'scale-high':2,
-		'6':2,
-		'dir':'anal'
 	
-	}
+if __name__ == '__main__':
+	
+	parser = argparse.ArgumentParser(
+	description=
+"""imsolve.py:
+This a barebones wrapper for astrometry.net. It has a few usefull parameters
+hard coded and it knows how to get the ra and dec from the azcam style headers
+that we are all used to.
+
+	Auther	:	Scott Swindell
+	Date		:	12 10 2015"""
+	)
+	
+
+	parser.add_argument('-o',  default='astrometry', help='Output directory for the solution files.')
+	parser.add_argument( '-e', type=int, default=-1, help='Which extension to solve default is all' )                  
 
 
+	group = parser.add_mutually_exclusive_group(required=True)
+	group.add_argument('-f', default=None,  help='fits file to solve')
+	group.add_argument( '-d',  default=None, help='Directory of data. All fits files will be solved' )
+	
 
-	cmd = do_astrometry(fname, p, ['overwrite', 'crpix-center'])
-	print cmd
+	args = parser.parse_args()
+	
+	astro_params = default_params
+	astro_flags = default_flags
+	
+	if  not os.path.exists(args.o):
+	
+		os.mkdir(args.o)
+	astro_params['dir'] = args.o
+		
+	if args.f:
+		bname = args.f.split( '/' )[-1]
+		if '.fits' == bname[-5:]:
+			bname = bname[:-5]
+			
+		img = fits.open(args.f)
+		
+		astro_params['ra'], astro_params['dec'] = getazcamradec( img )
+		
 
-	os.system(cmd)
+		
+		if args.e < 0:
+			exts = range( 1, len(img) )
+		else:
+			exts = [args.e]
+			
+		for ext in exts:
+			astro_params['6'] = ext
+			astro_params['o'] = "{0}_ext{1}".format( bname, ext )
+			cmd = astrometry_cmd( args.f, astro_params, astro_flags )
+			print cmd
+			
+			os.system(cmd)
+	
+	else:
+		
+		fnames = os.listdir( args.d )
+		for fname in fnames:
+			
+			if '.fits' == fname[-5:]:
+				bname = fname[:-5]
+			path_file = "{0}/{1}".format( args.d, fname )
+			img = fits.open( path_file )
+			astro_params['ra'], astro_params['dec'] = getazcamradec( img )	
+			
+			if args.e < 0:
+				exts = range( 1, len(img) )
+			else:
+				exts = [args.e]
+				
+			
+			for ext in exts:
+				astro_params['6'] = ext
+				astro_params['o'] = "{0}_ext{1}".format( bname, ext )
+				cmd = astrometry_cmd( path_file, astro_params, astro_flags )
+				print cmd
+			
+				os.system(cmd)
+
+

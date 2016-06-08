525c6e4bea13dbd146b52ff2a6d9ed43b7e7a7b7
scott <scottswindell@email.arizona.edu>
2016-06-08 14:54:06 -0700

    Added a solvefitsfd function to m4k_imclient so that you can solve an image by passing a fits HDUlist rather than a file name. It also returns a dictionary of values and HDUlist data types rather than saving them to disk.

--- src/fits_solver/m4k_imclient.py
+++ src/fits_solver/m4k_imclient.py
@@ -5,8 +5,92 @@ from source_extraction import *
 import tempfile
 import sys
 import os
+import tempfile
+
+def solvefitsfd( img, extnum=0, port=9002 ):
+	objs = getobjects( img[extnum].data )
+	#set up the tblargs
+	ra, dec = img[0].header['ra'] , img[0].header['dec']
+	naxis1, naxis2 = img[extnum].header['naxis1'], img[extnum].header['naxis2']
+
+	tblargs ={
+		'ra':ra,
+		'dec':dec,
+		'npix1':naxis1,
+		'npix2':naxis2,
+	}	
+	
+
+		
+	fitstbl = mkfitstable( objs )
+	for key, val in tblargs.iteritems():
+		fitstbl.header[key] = val
+	tname = tempfile.mktemp()
+	fitstbl.writeto( tname )
 
 
+	fitstbl_fd=open(tname, 'rb')
+	
+	soc = scottSock( "nimoy", port  )
+
+	soc.send( fitstbl_fd.read() )
+	while 1:
+		try:
+			meta = soc.recv( 256 )
+			break
+		except Exception:
+			pass
+
+	try:
+		meta = json.loads( meta )
+	except Exception as err:
+		return {}
+
+
+
+	outfits = {}
+	for fname in meta['forder']:
+		fsize = meta['files'][fname]
+		data = ''		
+		buffsize = 128
+		#print "File is {} filesize if {}".format(fname,fsize)
+		while 1:
+
+			if fsize > buffsize:
+				newData = soc.recv( buffsize )
+				
+				if len(newData) == 0:
+					#print "Soc recieved no data for file {}".format(fname)
+					#print "Buffer size is {} file has {} bytes left, data is {} bytes".format(buffsize, fsize, len(data))
+					break
+				
+				data+=newData
+				
+				fsize-=len(newData)
+			else:
+				data+=soc.recv(fsize)
+				break
+			
+		tempname = tempfile.mktemp()
+
+		tmpfd = open( tempname, 'wb' )
+		
+		tmpfd.write( data )
+		tmpfd.close()
+		try:
+			outfits[fname] = fits.open( tempname )
+		except Exception as err:
+			print "The file {} was not downloaded error was {}".format( fname, err )
+
+		os.remove(tempname)
+
+	for key, val in meta.iteritems():
+		if str(key) != "files" and str(key) != "forder":
+			outfits[key] = val
+
+	return outfits
+		
+		
 def main( imgname="test0021.fits", inDir='/home/bigobs/data/scott/26april16', outDir=None, extnum=1, port=9002, **headervals):
 
 
@@ -16,10 +100,7 @@ def main( imgname="test0021.fits", inDir='/home/bigobs/data/scott/26april16', ou
 	else:
 		outDir = "{0}/astrometry".format(inDir)
 
-	
-	
 
-	
 
 	if not os.path.exists(outDir):
 		os.makedirs(outDir)
